d57e468df97177d0b0b22e63b1141becf67f32d3
AlexeyPashkevich <gamevirus2015@gmail.com>
2021-02-13 15:49:44 +0200

    added product

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function () {
 
+  $(".weekly-products__rate").rateYo({
+    rating: 4.5,
+    readOnly: true,
+    ratedFill: "#ffc000",
+    starWidth: "14px",
+    spacing: "3px",
+    normalFill: "#dedede",
+  });
 
+
+  $('.weekly-products__wrapper').slick({
+    prevArrow: '<button type="button" class="slick-arrow slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-arrow slick-next"></button>',
+  });
 
 });
